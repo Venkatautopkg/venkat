3ff5e8e97a44fffc58f79b0d689ee8f482574512
jgstew <james@jgstew.com>
2021-11-08 15:06:04 -0500

    copy results to folder

--- build-recipes-all.py
+++ build-recipes-all.py
@@ -9,6 +9,9 @@ import random
 import subprocess
 import sys
 import time
+import shutil
+import re
+import pathlib
 
 import yaml
 
@@ -16,9 +19,11 @@ datetime_NOW = datetime.datetime.now()
 
 
 def get_all_files(extension=".bigfix.recipe.yaml"):
+    """get all files of a specific extension in folder"""
     # https://stackoverflow.com/a/3964691/861745
     print("get_all_files()")
     file_paths = []
+    # use current working directory
     for root, _, files in os.walk("."):
         for file in files:
             if file.endswith(extension):
@@ -28,6 +33,7 @@ def get_all_files(extension=".bigfix.recipe.yaml"):
 
 
 def get_recipe_identifier(recipe_path):
+    """read a recipe identifier from a yaml recipe"""
     # print("get_recipe_identifier(recipe_path)")
     with open(recipe_path, "rb") as stream:
         try:
@@ -56,6 +62,7 @@ def get_recipe_identifier(recipe_path):
 
 
 def get_all_identifiers(recipe_path_array):
+    """get all recipe identifiers from yaml recipe path array"""
     print("get_all_identifiers(recipe_path_array)")
     recipe_identifiers = []
     for path in recipe_path_array:
@@ -67,7 +74,37 @@ def get_all_identifiers(recipe_path_array):
     return recipe_identifiers
 
 
+def copy_update_fixlet(raw_output):
+    """copy generated fixlet to folder"""
+    # print("copy_update_fixlet(raw_output)")
+
+    cwd_dir = os.getcwd()
+    parent_dir = os.path.abspath(os.path.join(cwd_dir, os.pardir))
+    parent_dir_pathlib = pathlib.Path(parent_dir)
+
+    # this basically hard codes a destination folder:
+    dst_dir_pathlib = parent_dir_pathlib / "updates-for-win-apps" / "fixlets"
+    dst_dir_pathstring = dst_dir_pathlib.__str__()
+
+    if not os.path.isdir(dst_dir_pathstring):
+        print(f"WARNING: destination folder not found! {dst_dir_pathstring}")
+        return None
+
+    # print(raw_output)
+
+    re_update_filepath = "ContentFromTemplate: +Content File += +(.*-Update.bes)"
+
+    update_filepath = re.search(re_update_filepath, raw_output).group(1)
+
+    # print(update_filepath)
+
+    if os.path.isfile(update_filepath):
+        # copy file found in output to destination folder
+        return shutil.copy(update_filepath, dst_dir_pathstring)
+
+
 def run_recipe(recipe_id):
+    """actually run a recipe by identifier"""
     print("run_recipe(recipe_id)")
     # print(recipe_id)
     run_cmd = ["python", "../autopkg/Code/autopkg", "run", "-v", recipe_id]
@@ -77,11 +114,14 @@ def run_recipe(recipe_id):
     if "Receipt written to" in run_output:
         print(f"Completed: {recipe_id}")
         print(run_output.split("Receipt written to", 1)[1].split("\n", 1)[1])
+        if "ContentFromTemplate: Content File" in run_output:
+            copy_update_fixlet(run_output)
     else:
         print(f"ERROR: {recipe_id}")
 
 
 def run_all_recipes(recipe_identifiers):
+    """Run all recipe identifiers in an array"""
     print("run_all_recipes()")
     print(f"Number of Recipes to Run: {len(recipe_identifiers)}")
 
@@ -91,11 +131,12 @@ def run_all_recipes(recipe_identifiers):
 
     for recipe_id in recipe_identifiers:
         run_recipe(recipe_id)
-        # sleep 2 seconds between each
-        time.sleep(2)
+        # sleep 1 seconds between each
+        time.sleep(1)
 
 
 def get_last_runtime_recipe(recipe_identifier):
+    """Determine the last time a specific recipe has been run"""
     # print(recipe_identifier)
     recipe_receipt_dir = os.path.expanduser(
         f"~/Library/AutoPkg/Cache/{recipe_identifier}/receipts"
@@ -110,6 +151,7 @@ def get_last_runtime_recipe(recipe_identifier):
 
 
 def run_first_or_oldreceipt_recipes(recipe_identifiers, min_age_hours=6):
+    """run recipes that have never been run, or last run min_age_hours ago"""
     # print function name:
     print(sys._getframe().f_code.co_name + "()")
     print(f"Total Recipes: {len(recipe_identifiers)}")
@@ -139,6 +181,7 @@ def run_first_or_oldreceipt_recipes(recipe_identifiers, min_age_hours=6):
 
 
 def main():
+    """Execution Starts Here"""
     print("main()")
     recipe_path_array = get_all_files()
     recipe_identifiers = get_all_identifiers(recipe_path_array)
