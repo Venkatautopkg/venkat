1a24b1959fa8089d9a8014884303a413ca9626b0
jgstew <james@jgstew.com>
2021-11-29 17:33:20 -0500

    working to assign product ids to a single yaml file. Need to test with others before applying to all

--- .assign_product_ids.py
+++ .assign_product_ids.py
@@ -4,7 +4,24 @@ assign unique content_product_id to bigfix.recipie.yaml files in repo if not fou
 
 import os
 
+import ruamel.yaml
 import yaml
+
+
+def patch_ruamel_yaml_emitter_no_blank_lines():
+    """change ruamel.yaml to not produce blank lines after comments"""
+    # https://stackoverflow.com/a/53890725/861745
+    ruamel.yaml.emitter.Emitter.old_write_comment = (
+        ruamel.yaml.emitter.Emitter.write_comment
+    )
+
+    def write_comment(self, comment, *args, **kwargs):
+        # print("{:02d} {:02d} {!r}".format(self.column, comment.start_mark.column, comment.value))
+        comment.value = comment.value.replace("\r\n", "\n")
+        if comment.value.strip():
+            self.old_write_comment(comment, *args, **kwargs)
+
+    ruamel.yaml.emitter.Emitter.write_comment = write_comment
 
 
 def get_vendor_folders(root_folder):
@@ -43,13 +60,53 @@ def get_folder_product_ids(folder_item):
     product_ids = []
 
     for item in os.listdir(folder_item):
-        if os.path.isfile(os.path.join(folder_item, item)) and item.endswith(
-            ".bigfix.recipe.yaml"
-        ):
-            print(item)
-            product_id = get_file_product_id(os.path.join(folder_item, item))
+        item_path = os.path.join(folder_item, item)
+        if os.path.isfile(item_path) and item.endswith(".bigfix.recipe.yaml"):
+            product_id = get_file_product_id(item_path)
             if product_id:
                 product_ids.append(product_id)
+
+    return product_ids
+
+
+def write_missing_product_id(file_item, next_id):
+    """write product id into yaml file"""
+    print("00" + str(next_id))
+    print(file_item)
+    ruamel_yaml = ruamel.yaml.YAML()
+    # keep current quotes in place:
+    ruamel_yaml.preserve_quotes = True
+    # add / keep starting `---` at top of file
+    ruamel_yaml.explicit_start = True
+    # https://stackoverflow.com/a/44389139/861745
+    ruamel_yaml.indent(mapping=2, sequence=4, offset=2)
+
+    with open(file_item, "rb") as stream:
+        yaml_data = ruamel_yaml.load(stream)
+    yaml_data["Input"]["content_id_product"] = "00" + str(next_id)
+    print(yaml_data["Input"])
+    with open(file_item, "w") as f:
+        ruamel_yaml.dump(yaml_data, f)
+    return next_id
+
+
+def write_missing_product_ids(folder_item):
+    """write missing content_id_products for bigfix recipes in a vendor folder"""
+
+    product_ids = get_folder_product_ids(folder_item)
+    if len(product_ids) == 0:
+        product_ids.append(0)
+
+    for item in os.listdir(folder_item):
+        item_path = os.path.join(folder_item, item)
+        if (
+            os.path.isfile(item_path)
+            and item.endswith(".bigfix.recipe.yaml")
+            and not get_file_product_id(item_path)
+        ):
+            product_ids.append(
+                write_missing_product_id(item_path, max(product_ids) + 1)
+            )
 
     return product_ids
 
@@ -57,14 +114,17 @@ def get_folder_product_ids(folder_item):
 def main(root_folder=None):
     """Execution starts here"""
     print("main()")
+
+    patch_ruamel_yaml_emitter_no_blank_lines()
+
     if not root_folder:
         root_folder = os.path.abspath(os.path.curdir)
 
     vendor_folders = get_vendor_folders(root_folder)
     # print(vendor_folders)
 
-    print(get_folder_product_ids(vendor_folders[0]))
-    print(get_folder_product_ids(vendor_folders[1]))
+    print(write_missing_product_ids(vendor_folders[0]))
+    print(write_missing_product_ids(vendor_folders[1]))
 
 
 if __name__ == "__main__":
